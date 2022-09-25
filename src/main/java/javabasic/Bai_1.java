f41b5b9ce8a0b4c1a4907b51f0b5720e630eb9fd
chieudo <chieutk102@gmail.com>
2022-09-25 17:11:13 +0700

    bai1, 2

---
+++ src/main/java/javabasic/Bai_1.java
@@ -0,0 +1,17 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class Bai_1 {
+    public static void main(String[] args) {
+//        int number;
+        System.out.println("Nhap vao mot so: ");
+        Scanner a = new Scanner(System.in);
+        int number = a.nextInt();
+        if (number > 0) {
+            System.out.println("so " +number+"  la so nguyen duong");
+        } else {
+            System.out.println("so " +number+ " la so nguyen am");
+        }
+    }
+}
---
+++ src/main/java/javabasic/Bai_2.java
@@ -0,0 +1,28 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class Bai_2 {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.println("Nhap vao 3 so: ");
+        System.out.println("Nhap vao so a: ");
+        Double a = number.nextDouble();
+        System.out.println("Nhap vao so b: ");
+        Double b = number.nextDouble();
+        System.out.println("Nhap vao so c: ");
+        Double c = number.nextDouble();
+        if (a+b>c && a+c>b && b+c>a) {
+            System.out.println("Day la 3 canh cua 1 tam giac");
+            if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b+c*c==a*a)) {
+                System.out.println("Day la tam giac vuong");
+            } else {
+                System.out.println("day khong la tam giac vuong");
+            }
+        } else {
+            System.out.println("day khong phai la ba canh cua 1 tam giac");
+        }
+
+    }
+}
+
