4a6f32aac267fcb0b55ae4f860e2be97ca0950e7
TrangLe22072000 <trangle22072k@gmail.com>
2022-09-27 00:26:50 +0700

    baitap

---
+++ src/main/java/javabasic/Bai_5_Topic1.java
@@ -0,0 +1,19 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class Bai_5_Topic1 {
+    public static void main(String[] args)
+    {
+        String chuoi;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập vào một chuỗi: ");
+        chuoi = sc.nextLine();
+
+        if (chuoi.matches(".*\\d.*"))
+            System.out.println("Có");
+        else
+            System.out.println("Không");
+    }
+}
