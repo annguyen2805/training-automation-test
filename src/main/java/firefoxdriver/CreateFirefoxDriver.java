35b721577e3dc009dac41bf7295009bcd13775f8
Chieu Do <chieutk102@gmail.com>
2022-10-06 10:45:14 +0700

    webdriver

--- src/main/java/firefoxdriver/CreateFirefoxDriver.java
+++ src/main/java/firefoxdriver/CreateFirefoxDriver.java
@@ -1,5 +1,6 @@
 package firefoxdriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverInfo;
@@ -7,6 +8,7 @@ import org.openqa.selenium.firefox.GeckoDriverInfo;
 public class CreateFirefoxDriver {
     public static void main(String[] args) {
         WebDriver driver;
+//        WebDriverManager.firefoxdriver().setup();
         System.setProperty("webdriver.gecko.driver","C:\\autotest\\chieudo\\training-automation-test\\src\\main\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
--- src/main/java/firefoxdriver/CreateFirefoxDriverManager.java
+++ src/main/java/firefoxdriver/CreateFirefoxDriverManager.java
@@ -4,8 +4,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.sql.Driver;
+
 public class CreateFirefoxDriverManager {
-//    WebDriver driver;
-    FirefoxDriver driver;
-//    WebDriverManager.firefoxdriver()
+    public static void main(String[] args) {
+        WebDriver driver;
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+        String title = new driver.getTitle();
+        System.out.println("Title" + title);
+    }
 }
